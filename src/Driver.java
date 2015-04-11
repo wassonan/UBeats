f22b477f0c19e50ee179a8a4ad929ccb8e3b8e45
Wasson An <wassonan257@gmail.com>
2015-04-11 13:01:21 -0400

    stuff

--- src/Driver.java
+++ src/Driver.java
@@ -7,7 +7,9 @@ public class Driver {
 	//The main function that initiated our program
 	public static void main(String[] args){
 		
+		System.load(System.getProperty("user.dir") + "/libs/libopencv_java2411.dylib");
 		System.out.println("Starting Program");
+		ScanTest.scan();
 		MainFrame mf = new MainFrame();
 	} //main
 } //Driver
