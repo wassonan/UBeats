ee66bc7348ac419d2774d2169632112b67e7866a
Guru Ram Ambalavanar <grambalavanar@gmail.com>
2015-04-10 22:43:15 -0400

    began testing cv options

--- src/CVTest.java
+++ src/CVTest.java
@@ -1,9 +1,14 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import com.googlecode.javacv.cpp.opencv_highgui.*;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 public class CVTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IplImage img = imread("base.jpg");
+		Mat mtx(img); // convert IplImage* -> Mat
+		absdiff(prev_frame, next_frame, d1);
 	}
 
 }
